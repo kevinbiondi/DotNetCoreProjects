7cdfc1120421d4891c53559b770ed3cd4343ba45
Kevin Biondi <kbiondi@live.com>
2020-03-22 20:03:45 -0400

    Revert "Phase 3: Editing (updating and adding records) data with Razor Pages"
    This reverts commit bba0e9c34f61e225365740ecaaea48a43c33012c.

--- OdeToFood/OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -1,5 +1,7 @@
 ﻿using OdeToFood.Core;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace OdeToFood.Data
@@ -11,7 +13,67 @@ namespace OdeToFood.Data
         Restaurant GetById(int id);
         Restaurant Update(Restaurant updatedRestaurant);
         Restaurant Add(Restaurant newRestaurant);
-        Restaurant Delete(int id);
         int Commit();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData() 
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Location="Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Jersey Mikes", Location="Virginia", Cuisine=CuisineType.None},
+                new Restaurant {Id = 3, Name = "Mint", Location="Virginia", Cuisine=CuisineType.Indian}
+            };
+
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+
+
+        }
+        public IEnumerable<Restaurant>GetRestaurantsByName(string searchTerm)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(searchTerm) || r.Name.StartsWith(searchTerm)
+                   orderby r.Name
+                   select r;
+
+
+        }
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {           
+            //only for test and development... will take out when adding database
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
     }
 }
--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,16 +24,8 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<OdeToFoodDbContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
-            });
-
             //Biondi--added only for the canned data for data-remove when adding a database
-            //services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
-
-            //Biondi --added to switch over to use SqlRestaurantData
-            services.AddScoped<IRestaurantData, SqlRestaurantData>();
+            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
 
             services.AddRazorPages();
         }
