1dded5506215996c07a80261fc945a7963c94339
Kevin Biondi <kbiondi@live.com>
2020-03-31 12:29:11 -0400

    Added Lambdas, Action<T>, and Func<T,Result> code

---
+++ delegates-pluralsight/delegates-pluralsight/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace delegates_pluralsight
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
--- delegates-pluralsight/delegates-pluralsight/Program.cs
+++ delegates-pluralsight/delegates-pluralsight/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace delegates_pluralsight
 {
-
+    public delegate int BizRulesDelegate(int x, int y);
     class Program
     {
         public static void Main(string[] args)
@@ -28,6 +30,48 @@ namespace delegates_pluralsight
             //one point of reference to invoke mulitiple subscribers
             //del1(10, WorkType.GenerateReports);
 
+
+            var custs = new List<Customer>
+            {
+                new Customer {City = "King George", FirstName = "Kevin", LastName = "Biondi", ID=1},
+            new Customer { City = "King George", FirstName = "Connor", LastName = "Biondi", ID = 2 },
+            new Customer { City = "Richmond", FirstName = "Jody", LastName = "DesRoches", ID = 3 },
+            new Customer { City = "Pittsburgh", FirstName = "Ben", LastName = "Roethisberger", ID = 5 },
+            new Customer { City = "King George", FirstName = "Sydney", LastName = "Biondi", ID = 500 }
+            };
+
+            var kgCustomers = custs.Where(c => c.City == "King George" && c.ID < 500)
+                                    .OrderBy(c => c.FirstName);
+            foreach (var cust in kgCustomers)
+            {
+                Console.WriteLine(cust.FirstName);
+            }
+
+
+
+
+
+            var data = new ProcessData();
+            BizRulesDelegate addDel = (x, y) => x + y;
+            BizRulesDelegate multiplyDel = (x, y) => x * y;
+            //Note:  Process doesn't know until runtime which business rules to call: addDel or multiplyDel
+            //There could be an if statement that determines whether addDel or mulitplyDel gets called
+            //This is dynamic at runtime
+            data.Process(2, 3, addDel);
+
+            //The following does the same as above but you don;t need to declare BizRulesDelegate like above
+            Func<int, int, int> funcAddDel = (x, y) => x + y;
+            Func<int, int, int> funcMultDel = (x, y) => x * y;
+            data.ProcessFunc(5, 2, funcAddDel);
+
+
+
+            
+
+            //Action<int, int> myAction = (x, y) => Console.WriteLine(x + y);
+            Action<int, int> myMultiply = (x, y) => Console.WriteLine(x * y);
+            data.ProcessAction(5, 6, myMultiply);
+
             var worker = new Worker();
             //Defining and attaching event handlers OPTION 1
             //worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(worker_WorkPerformed);
@@ -36,8 +80,8 @@ namespace delegates_pluralsight
             //OR DO
 
             //Defining and attaching event handlers by Delegate Inference OPTION 2
-            worker.WorkPerformed += worker_WorkPerformed;
-            worker.WorkCompleted += worker_WorkCompleted;
+            //worker.WorkPerformed += worker_WorkPerformed;
+            //worker.WorkCompleted += worker_WorkCompleted;
             //test -= to remove(doesn't make sense here tho)
             //worker.WorkCompleted -= worker_WorkCompleted;
 
@@ -54,22 +98,34 @@ namespace delegates_pluralsight
             //    Console.WriteLine("Worker is done");
             //};
 
+
+            //OR DO
+            //As a lambda
+            //worker.WorkPerformed += worker_WorkPerformed;
+            //Note: commented out the callback method worker_WorkCompleted below since now here
+            worker.WorkPerformed += (s, e) =>
+            {
+                Console.WriteLine("Hours Worked: " + e.Hours + " " + e.WorkType);
+                Console.WriteLine("Demonstate a lambda doing an inline method in curly braces for work performed");
+            };
+            worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is done");
+
             worker.DoWork(8, WorkType.GoToMeetings);
 
             Console.Read();
         }
 
         //This method is a callback
-        private static void worker_WorkCompleted(object sender, EventArgs e)
-        {
-            Console.WriteLine("Worker is done");
-        }
+        //private static void worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine("Worker is done");
+        //}
 
         //This method is the callback
-        public static void worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
-        {
-            Console.WriteLine("Hours Worked: " + e.Hours + " " + e.WorkType);
-        }
+        //public static void worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        //{
+        //    Console.WriteLine("Hours Worked: " + e.Hours + " " + e.WorkType);
+        //}
 
         //public static void DoWork(WorkPerformedHandler del)
         //{
